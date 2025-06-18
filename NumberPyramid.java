738217e6db0dcc2fa57e71ab04eb929129107ccd
suresh_999 <babusureshoruganti@gmail.com>
2025-06-18 12:02:41 +0530

    Create NumberPyramid.java

---
+++ NumberPyramid.java
@@ -0,0 +1,16 @@
+public class NumberPyramid {
+    public static void main(String[] args) {
+        int levels = 5; // You can change this value
+
+        for (int i = 1; i <= levels; i++) {
+            // Print spaces
+            for (int j = 1; j <= levels - i; j++) {
+                System.out.print(" ");
+        }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print(i);
+            }
+System.out.println();
+        }
+    }
+}
